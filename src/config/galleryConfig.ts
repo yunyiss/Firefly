bcd6a7513dc2399144e205be795a87a0bb14e26f
yunyiss <wb5513@163.com>
2026-07-25 02:40:19 +0800

    chore: 清理动态并更新关于我、相册、打赏内容

--- src/config/galleryConfig.ts
+++ src/config/galleryConfig.ts
@@ -15,25 +15,26 @@ export const galleryConfig: GalleryConfig = {
 		// password: 访问密码，设置后需要输入密码才能查看相册内容（可选）
 		// passwordHint: 密码提示，设置后在输入密码错误时显示（可选，需配合password使用）
 		// 每添加一个数组项就相当于添加了一个相册，记得在 public/gallery/ 目录下创建对应的子目录并放入图片
-		{
-			id: "firefly-2026",
-			name: "可爱流萤",
-			description: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
-			location: "崩坏：星穹铁道",
-			date: "2026-01-01",
-			tags: ["崩坏星穹铁道", "流萤"],
-		},
-		{
-			id: "encrypted-test",
-			name: "加密相册示例",
-			description:
-				"这是一个加密相册的示例，设置了访问密码，只有输入正确的密码才能查看相册内容。",
-			location: "崩坏：星穹铁道",
-			date: "2026-02-01",
-			tags: ["加密相册", "示例"],
-			password: "123456",
-			passwordHint: "示例密码123456",
-		},
+		
+		// {
+		// 	id: "firefly-2026",
+		// 	name: "可爱流萤",
+		// 	description: "飞萤之火自无梦的长夜亮起，绽放在终竟的明天。",
+		// 	location: "崩坏：星穹铁道",
+		// 	date: "2026-01-01",
+		// 	tags: ["崩坏星穹铁道", "流萤"],
+		// },
+		// {
+		// 	id: "encrypted-test",
+		// 	name: "加密相册示例",
+		// 	description:
+		// 		"这是一个加密相册的示例，设置了访问密码，只有输入正确的密码才能查看相册内容。",
+		// 	location: "崩坏：星穹铁道",
+		// 	date: "2026-02-01",
+		// 	tags: ["加密相册", "示例"],
+		// 	password: "123456",
+		// 	passwordHint: "示例密码123456",
+		// },
 	],
 
 	// 瀑布流最小列宽(px)，浏览器根据容器宽度自动计算列数，默认 240
--- src/config/sponsorConfig.ts
+++ src/config/sponsorConfig.ts
@@ -26,7 +26,7 @@ export const sponsorConfig: SponsorConfig = {
 			name: "支付宝",
 			icon: "fa7-brands:alipay",
 			// 收款码图片路径（需要放在 public 目录下）
-			qrCode: "/assets/images/sponsor/alipay.png",
+			qrCode: "/aassets/images/sponsor/alipay",
 			link: "",
 			description: "使用 支付宝 扫码打赏",
 			enabled: true,
@@ -34,25 +34,9 @@ export const sponsorConfig: SponsorConfig = {
 		{
 			name: "微信",
 			icon: "fa7-brands:weixin",
-			qrCode: "/assets/images/sponsor/wechat.png",
+			qrCode: "/aassets/images/sponsor/wechat",
 			link: "",
 			description: "使用 微信 扫码打赏",
-			enabled: true,
-		},
-		{
-			name: "ko-fi",
-			icon: "simple-icons:kofi",
-			qrCode: "",
-			link: "https://ko-fi.com/cuteleaf",
-			description: "Buy a Coffee for Firefly",
-			enabled: true,
-		},
-		{
-			name: "爱发电",
-			icon: "simple-icons:afdian",
-			qrCode: "",
-			link: "https://ifdian.net/a/cuteleaf",
-			description: "通过 爱发电 进行打赏",
 			enabled: true,
 		},
 	],
@@ -60,20 +44,20 @@ export const sponsorConfig: SponsorConfig = {
 	// 打赏者列表（可选）
 	sponsors: [
 		// 示例：已实名打赏者
-		{
-			name: "夏叶",
-			avatar:
-				"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
-			amount: "¥50",
-			date: "2025-10-01",
-		},
+		// {
+		// 	name: "夏叶",
+		// 	avatar:
+		// 		"https://weavatar.com/avatar/d252655d40d6874417a720bad0a6c5f77f8f6a1fd2f882f8f338402dc37e4190?s=640",
+		// 	amount: "¥50",
+		// 	date: "2025-10-01",
+		// },
 
 		// 示例：匿名打赏者
-		{
-			name: "匿名用户",
-			// avatar: "",
-			amount: "¥20",
-			date: "2025-10-01",
-		},
+		// {
+		// 	name: "匿名用户",
+		// 	// avatar: "",
+		// 	amount: "¥20",
+		// 	date: "2025-10-01",
+		// },
 	],
 };
